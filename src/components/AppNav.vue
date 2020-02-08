b6fc049b4682c677e6eb0cec5c8a33a066e0b139
Dan Morales <danmorales209@gmail.com>
2020-02-08 11:10:12 -0800

    Init

---
+++ src/components/AppNav.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="main-nav">
+        <ul>
+            <li class="main-nav-item">Home</li>
+            <li class="main-nav-item">Pantry</li>
+            <li class="main-nav-item">Users</li>
+        </ul>
+
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: "MainNav"
+
+}
+</script>
+
+<style scoped>
+
+</style>
